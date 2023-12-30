5f9aa456afefd5f63a2d2b08c806738d40d2f6bb
saravana4yu <saravanananthaa@gmail.com.com>
2023-12-30 12:59:20 +0530

    initial code

--- src/main/java/com/github/filemanager/FileManager.java
+++ src/main/java/com/github/filemanager/FileManager.java
@@ -1,7 +1,369 @@
 package com.github.filemanager;
 
-public class FileManager {
-    public static void main(String[] args) {
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.table.*;
+import javax.swing.tree.*;
+import org.apache.commons.io.FileUtils;
 
+public class FileManager {
+    public static final String APP_TITLE = "FatMan";
+    private Desktop desktop;
+    private FileSystemView fileSystemView;
+    private File currentFile;
+    private JPanel gui;
+    private JTree tree;
+    private DefaultTreeModel treeModel;
+    private JTable table;
+    private JProgressBar progressBar;
+    private FileTableModel fileTableModel;
+    private ListSelectionListener listSelectionListener;
+    private boolean cellSizesSet = false;
+    private int rowIconPadding = 6;
+
+    /* File controls. */
+    private JButton openFile;
+    private JButton printFile;
+    private JButton editFile;
+    private JButton deleteFile;
+    private JButton newFile;
+    private JButton copyFile;
+    /* File details. */
+    private JLabel fileName;
+    private JTextField path;
+    private JLabel date;
+    private JLabel size;
+    private JCheckBox readable;
+    private JCheckBox writable;
+    private JCheckBox executable;
+    private JRadioButton isDirectory;
+    private JRadioButton isFile;
+
+    /* GUI options/containers for new File/Directory creation.  Created lazily. */
+    private JPanel newFilePanel;
+    private JRadioButton newTypeFile;
+    private JTextField name;
+
+    public Container getGui() {
+        if (gui == null) {
+            gui = new JPanel(new BorderLayout(3, 3));
+            gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+            fileSystemView = FileSystemView.getFileSystemView();
+            desktop = Desktop.getDesktop();
+
+            JPanel detailView = new JPanel(new BorderLayout(3, 3));
+            // fileTableModel = new FileTableModel();
+
+            table = new JTable();
+            table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            table.setAutoCreateRowSorter(true);
+            table.setShowVerticalLines(false);
+
+            listSelectionListener =
+                    new ListSelectionListener() {
+                        @Override
+                        public void valueChanged(ListSelectionEvent lse) {
+                            int row = table.getSelectionModel().getLeadSelectionIndex();
+                            setFileDetails(((FileTableModel) table.getModel()).getFile(row));
+                        }
+                    };
+            table.getSelectionModel().addListSelectionListener(listSelectionListener);
+            JScrollPane tableScroll = new JScrollPane(table);
+            Dimension d = tableScroll.getPreferredSize();
+            tableScroll.setPreferredSize(
+                    new Dimension((int) d.getWidth(), (int) d.getHeight() / 2));
+            detailView.add(tableScroll, BorderLayout.CENTER);
+
+            // the File tree
+            DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+            treeModel = new DefaultTreeModel(root);
+
+            TreeSelectionListener treeSelectionListener =
+                    new TreeSelectionListener() {
+                        public void valueChanged(TreeSelectionEvent tse) {
+                            DefaultMutableTreeNode node =
+                                    (DefaultMutableTreeNode) tse.getPath().getLastPathComponent();
+                            showChildren(node);
+                            setFileDetails((File) node.getUserObject());
+                        }
+                    };
+
+            // show the file system roots.
+            File[] roots = fileSystemView.getRoots();
+            for (File fileSystemRoot : roots) {
+                DefaultMutableTreeNode node = new DefaultMutableTreeNode(fileSystemRoot);
+                root.add(node);
+                // showChildren(node);
+                File[] files = fileSystemView.getFiles(fileSystemRoot, true);
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        node.add(new DefaultMutableTreeNode(file));
+                    }
+                }
+            }
+
+            tree = new JTree(treeModel);
+            tree.setRootVisible(false);
+            tree.addTreeSelectionListener(treeSelectionListener);
+            tree.setCellRenderer(new FileTreeCellRenderer());
+            tree.expandRow(0);
+            JScrollPane treeScroll = new JScrollPane(tree);
+
+            // as per trashgod tip
+            tree.setVisibleRowCount(15);
+
+            Dimension preferredSize = treeScroll.getPreferredSize();
+            Dimension widePreferred = new Dimension(200, (int) preferredSize.getHeight());
+            treeScroll.setPreferredSize(widePreferred);
+
+            // details for a File
+            JPanel fileMainDetails = new JPanel(new BorderLayout(4, 2));
+            fileMainDetails.setBorder(new EmptyBorder(0, 6, 0, 6));
+
+            JPanel fileDetailsLabels = new JPanel(new GridLayout(0, 1, 2, 2));
+            fileMainDetails.add(fileDetailsLabels, BorderLayout.WEST);
+
+            JPanel fileDetailsValues = new JPanel(new GridLayout(0, 1, 2, 2));
+            fileMainDetails.add(fileDetailsValues, BorderLayout.CENTER);
+
+            fileDetailsLabels.add(new JLabel("File", JLabel.TRAILING));
+            fileName = new JLabel();
+            fileDetailsValues.add(fileName);
+            fileDetailsLabels.add(new JLabel("Path/name", JLabel.TRAILING));
+            path = new JTextField(5);
+            path.setEditable(false);
+            fileDetailsValues.add(path);
+            fileDetailsLabels.add(new JLabel("Last Modified", JLabel.TRAILING));
+            date = new JLabel();
+            fileDetailsValues.add(date);
+            fileDetailsLabels.add(new JLabel("File size", JLabel.TRAILING));
+            size = new JLabel();
+            fileDetailsValues.add(size);
+            fileDetailsLabels.add(new JLabel("Type", JLabel.TRAILING));
+
+            JPanel flags = new JPanel(new FlowLayout(FlowLayout.LEADING, 4, 0));
+            isDirectory = new JRadioButton("Directory");
+            isDirectory.setEnabled(false);
+            flags.add(isDirectory);
+
+            isFile = new JRadioButton("File");
+            isFile.setEnabled(false);
+            flags.add(isFile);
+            fileDetailsValues.add(flags);
+
+            int count = fileDetailsLabels.getComponentCount();
+            for (int ii = 0; ii < count; ii++) {
+                fileDetailsLabels.getComponent(ii).setEnabled(false);
+            }
+
+            JToolBar toolBar = new JToolBar();
+            // mnemonics stop working in a floated toolbar
+            toolBar.setFloatable(false);
+
+            openFile = new JButton("Open");
+            openFile.setMnemonic('o');
+
+            openFile.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent ae) {
+                            try {
+                                desktop.open(currentFile);
+                            } catch (Throwable t) {
+                                showThrowable(t);
+                            }
+                            gui.repaint();
+                        }
+                    });
+            toolBar.add(openFile);
+
+            editFile = new JButton("Edit");
+            editFile.setMnemonic('e');
+            editFile.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent ae) {
+                            try {
+                                desktop.edit(currentFile);
+                            } catch (Throwable t) {
+                                showThrowable(t);
+                            }
+                        }
+                    });
+            toolBar.add(editFile);
+
+            printFile = new JButton("Print");
+            printFile.setMnemonic('p');
+            printFile.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent ae) {
+                            try {
+                                desktop.print(currentFile);
+                            } catch (Throwable t) {
+                                showThrowable(t);
+                            }
+                        }
+                    });
+            toolBar.add(printFile);
+
+            // Check the actions are supported on this platform!
+            openFile.setEnabled(desktop.isSupported(Desktop.Action.OPEN));
+            editFile.setEnabled(desktop.isSupported(Desktop.Action.EDIT));
+            printFile.setEnabled(desktop.isSupported(Desktop.Action.PRINT));
+
+            toolBar.addSeparator();
+
+            newFile = new JButton("New");
+            newFile.setMnemonic('n');
+            newFile.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent ae) {
+                            newFile();
+                        }
+                    });
+            toolBar.add(newFile);
+
+            copyFile = new JButton("Copy");
+            copyFile.setMnemonic('c');
+            copyFile.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent ae) {
+                            showErrorMessage("'Copy' not implemented.", "Not implemented.");
+                        }
+                    });
+            toolBar.add(copyFile);
+
+            JButton renameFile = new JButton("Rename");
+            renameFile.setMnemonic('r');
+            renameFile.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent ae) {
+                            renameFile();
+                        }
+                    });
+            toolBar.add(renameFile);
+
+            deleteFile = new JButton("Delete");
+            deleteFile.setMnemonic('d');
+            deleteFile.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent ae) {
+                            deleteFile();
+                        }
+                    });
+            toolBar.add(deleteFile);
+
+            toolBar.addSeparator();
+
+            readable = new JCheckBox("Read  ");
+            readable.setMnemonic('a');
+            // readable.setEnabled(false);
+            toolBar.add(readable);
+
+            writable = new JCheckBox("Write  ");
+            writable.setMnemonic('w');
+            // writable.setEnabled(false);
+            toolBar.add(writable);
+
+            executable = new JCheckBox("Execute");
+            executable.setMnemonic('x');
+            // executable.setEnabled(false);
+            toolBar.add(executable);
+
+            JPanel fileView = new JPanel(new BorderLayout(3, 3));
+
+            fileView.add(toolBar, BorderLayout.NORTH);
+            fileView.add(fileMainDetails, BorderLayout.CENTER);
+
+            detailView.add(fileView, BorderLayout.SOUTH);
+
+            JSplitPane splitPane =
+                    new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, treeScroll, detailView);
+            gui.add(splitPane, BorderLayout.CENTER);
+
+            JPanel simpleOutput = new JPanel(new BorderLayout(3, 3));
+            progressBar = new JProgressBar();
+            simpleOutput.add(progressBar, BorderLayout.EAST);
+            progressBar.setVisible(false);
+
+            gui.add(simpleOutput, BorderLayout.SOUTH);
+        }
+        return gui;
+    }
+
+    public void showRootFile() {
+        // ensure the main files are displayed
+        tree.setSelectionInterval(0, 0);
+    }
+
+    private TreePath findTreePath(File find) {
+        for (int ii = 0; ii < tree.getRowCount(); ii++) {
+            TreePath treePath = tree.getPathForRow(ii);
+            Object object = treePath.getLastPathComponent();
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode) object;
+            File nodeFile = (File) node.getUserObject();
+
+            if (nodeFile.equals(find)) {
+                return treePath;
+            }
+        }
+        // not found!
+        return null;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(
+                new Runnable() {
+                    public void run() {
+                        try {
+                            // Significantly improves the look of the output in
+                            // terms of the file names returned by FileSystemView!
+                            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                        } catch (Exception weTried) {
+                        }
+                        JFrame f = new JFrame(APP_TITLE);
+                        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                        FileManager fileManager = new FileManager();
+                        f.setContentPane(fileManager.getGui());
+
+                        try {
+                            URL urlBig = fileManager.getClass().getResource("fm-icon-32x32.png");
+                            URL urlSmall = fileManager.getClass().getResource("fm-icon-16x16.png");
+                            ArrayList<Image> images = new ArrayList<Image>();
+                            images.add(ImageIO.read(urlBig));
+                            images.add(ImageIO.read(urlSmall));
+                            f.setIconImages(images);
+                        } catch (Exception weTried) {
+                        }
+
+                        f.pack();
+                        f.setLocationByPlatform(true);
+                        f.setMinimumSize(f.getSize());
+                        f.setVisible(true);
+
+                        fileManager.showRootFile();
+                    }
+                });
     }
 }
+
+
